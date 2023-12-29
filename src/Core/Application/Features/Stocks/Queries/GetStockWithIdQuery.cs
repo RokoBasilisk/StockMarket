a5de234f945bd7dffa494b269a891f48a2873e9f
RokoBasilisk <dhnghia92tcv@gmail.com>
2023-12-29 13:26:29 +0700

    add Infrastructure and Setup Dtos

---
+++ src/Core/Application/Features/Stocks/Queries/GetStockWithIdQuery.cs
@@ -0,0 +1,22 @@
+﻿using Application.Dtos;
+using Application.Wrapper.Abstract;
+using Application.Wrapper.Concrete;
+using MediatR;
+
+namespace Application.Features.Stocks.Queries
+{
+    public class GetStockWithIdQuery : IRequest<DataResponse<IEnumerable<StockDto>>>
+    {
+        public class GetStockWithIdQueryHandler : IRequestHandler<GetStockWithIdQuery, DataResponse<IEnumerable<StockDto>>>
+        {
+            public GetStockWithIdQueryHandler() {
+            }
+            public Task<DataResponse<IEnumerable<StockDto>>> Handle(GetStockWithIdQuery request, CancellationToken cancellationToken)
+            {
+                
+            }
+        }
+
+
+    }
+}
